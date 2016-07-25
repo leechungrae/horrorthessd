eec12760ac7e3c55a5e5f059ad3155e7d0f173f5
leechungrae <leechungrae@gmail.com>
2016-07-25 17:00:11 +0900

    horror1

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,32 @@
+class WelcomeController < ApplicationController
+	def index
+  end
+  def pict_1
+  end
+  def pict_2
+  end
+  def pict_3
+  end
+  def pict_4
+  end
+  def pict_5
+  end
+  def pict_6
+  end
+  def pict_7
+  end
+  def pict_8
+  end
+  def pict_9
+  end
+  def pict_10
+  end
+  def pict_11
+  end
+  def pict_12
+  end
+  def pict_13
+  end
+  def pict_14
+  end
+end
